abf6cef87d07c2798d61f130463184c3a8f4f5f7
ricardofrance <ricardolfrance@gmail.com>
2021-12-13 21:28:57 -0300

    enviando arquivos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Ricardo Francé";
+var idade = 43;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);]
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
